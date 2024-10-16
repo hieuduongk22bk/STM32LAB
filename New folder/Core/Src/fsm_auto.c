b156ef20b01af7093ffc4ad03744bb8639e06a49
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2024-10-16 13:26:36 +0700

    no message

--- New folder/Core/Src/fsm_auto.c
+++ New folder/Core/Src/fsm_auto.c
@@ -8,34 +8,38 @@
 #include"fsm_auto.h"
 
 
-void fsm_auto_run(){
-switch(status){
+void fsm_auto_run() {
+	switch (status) {
 	case INIT:
 		//TODO INIT
-		if(1){
+		if (1) {
 			status = GREEN_RED;
-			setTimer(0,green_time);
+			local_green_time = green_time/1000;
+			local_red_time = red_time/1000;
+			local_amber_time = amber_time/1000;
+			setTimer(0, green_time);
 			button_flag[0] = 0; // chuan bi cho trang thai nut nhan, dam bao truoc trang thai GREEN_RED button=0 (vi GREEN_RED co doi nut nhan)
-			setTimer(2,10);
-			setTimer(3,1000);
+			setTimer(2, 10);
+			setTimer(3, 1000);
 		}
 		break;
 	case GREEN_RED:
 		led_green_red();
-		if(checkTimer(3)==1){
+		if (checkTimer(3) == 1) {
 			local_green_time--;
 			local_red_time--;
-			if(local_green_time <= 0) local_green_time = green_time;
-
+			if(local_green_time <= 0) local_green_time = green_time/1000;
+			if(local_red_time <= 0) local_red_time = red_time/1000;
+			setTimer(3, 1000);
 		}
-		if(checkTimer(2)==1){
+		if (checkTimer(2) == 1) {
 			//Update Display;
-			update7segBuffer(local_green_time,local_red_time);
-			setTimer(2,250);
+			update7segBuffer(local_green_time, local_red_time);
+			setTimer(2, 250);
 		}
-		if(checkTimer(0)==1){
+		if (checkTimer(0) == 1) {
 			status = AMBER_RED;
-			setTimer(0,amber_time);
+			setTimer(0, amber_time);
 		}
 //		if(button_flag[0]==1){
 //		status = MAN_GREEN_RED;
@@ -46,10 +50,22 @@ switch(status){
 		break;
 	case AMBER_RED:
 		led_amber_red();
-		if(checkTimer(0)==1){
+		if (checkTimer(0) == 1) {
 			status = RED_GREEN;
-			setTimer(0,green_time);
-				}
+			setTimer(0, green_time);
+		}
+		if (checkTimer(3) == 1) {
+			local_amber_time--;
+			local_red_time--;
+			if(local_amber_time <= 0) local_amber_time = amber_time/1000;
+			if(local_red_time <= 0) local_red_time = red_time/1000;
+			setTimer(3, 1000);
+		}
+		if (checkTimer(2) == 1) {
+			//Update Display;
+			update7segBuffer(local_amber_time , local_red_time);
+			setTimer(2, 250);
+		}
 		break;
 	case RED_GREEN:
 		led_red_green();
@@ -57,12 +73,36 @@ switch(status){
 			status = RED_AMBER;
 			setTimer(0, amber_time);
 		}
+		if (checkTimer(3) == 1) {
+			local_green_time--;
+			local_red_time--;
+			if(local_green_time <= 0) local_green_time = green_time/1000;
+			if(local_red_time <= 0) local_red_time = red_time/1000;
+			setTimer(3, 1000);
+		}
+		if (checkTimer(2) == 1) {
+			//Update Display;
+			update7segBuffer(local_red_time, local_green_time);
+			setTimer(2, 250);
+		}
 		break;
 	case RED_AMBER:
 		led_red_amber();
 		if (checkTimer(0) == 1) {
 			status = GREEN_RED;
 			setTimer(0, green_time);
+		}
+		if (checkTimer(3) == 1) {
+			local_amber_time--;
+			local_red_time--;
+			if(local_amber_time <= 0) local_amber_time = amber_time/1000;
+			if(local_red_time <= 0) local_red_time = red_time/1000;
+			setTimer(3, 1000);
+		}
+		if (checkTimer(2) == 1) {
+			//Update Display;
+			update7segBuffer(local_red_time, local_amber_time);
+			setTimer(2, 250);
 		}
 		break;
 	default:
