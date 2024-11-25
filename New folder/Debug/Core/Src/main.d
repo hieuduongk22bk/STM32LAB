58167713c4c629848019001a3a437054d7f1e3cb
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2024-11-25 14:05:19 +0700

    no message

--- New folder/Debug/Core/Src/main.d
+++ New folder/Debug/Core/Src/main.d
@@ -22,11 +22,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/scheduler.h ../Core/Inc/fsm_auto.h ../Core/Inc/global.h \
- ../Core/Inc/main.h ../Core/Inc/button.h ../Core/Inc/led_traffic.h \
- ../Core/Inc/7segment_bcd.h ../Core/Inc/fsm_man.h \
- ../Core/Inc/button_read.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
 
 ../Core/Inc/main.h:
 
@@ -77,21 +73,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
-
-../Core/Inc/scheduler.h:
-
-../Core/Inc/fsm_auto.h:
-
-../Core/Inc/global.h:
-
-../Core/Inc/main.h:
-
-../Core/Inc/button.h:
-
-../Core/Inc/led_traffic.h:
-
-../Core/Inc/7segment_bcd.h:
-
-../Core/Inc/fsm_man.h:
-
-../Core/Inc/button_read.h:
